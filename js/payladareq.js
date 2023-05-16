f14906c5c31d21897e6cec9f96b72244ad1c4133
Lucasadasc <71296540+Lucasadasc@users.noreply.github.com>
2023-05-16 11:54:14 -0300

    1605m1

---
+++ js/payladareq.js
@@ -0,0 +1,13 @@
+const url = "http://127.0.0.1:8000/paylada/jog/"
+
+const loading = document.querySelector("#loading")
+const conteudo = document.querySelector("#container-fluid")
+
+//Pegando todos os jogadores
+async function getAllJogadores(){ //async - vou usar await para esperar as requisições
+    const response = await fetch(url)
+
+    console.log(response)
+}
+
+getAllJogadores()

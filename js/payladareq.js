039e2fc94e9087bdec871aa99bc23fe196971b84
Lucasadasc <71296540+Lucasadasc@users.noreply.github.com>
2023-05-17 17:52:00 -0300

    1705t1

--- js/payladareq.js
+++ js/payladareq.js
@@ -7,11 +7,13 @@ const conteudo = document.querySelector("#container-fluid")
 async function getAllJogadores(){ //async - vou usar await para esperar as requisições
     const response = await fetch(url)
 
-    console.log(response)
-
     const jogadores = await response.json()
 
     console.log(jogadores)
+
+    loading.classList.add("hide")
+
+    jogadores.map
 }
 
 getAllJogadores()

e16c2bf509b0405e7ea1417322b572c4f23e361b
Lucasadasc <71296540+Lucasadasc@users.noreply.github.com>
2023-06-16 20:33:40 -0300

    1606n2

--- js/fev.js
+++ js/fev.js
@@ -14,7 +14,9 @@ const jogpay = document.querySelector("#jogadorpay");
 const urlsearchParams = new URLSearchParams(window.location.search) //me entrega um objeto que eu posso acessar os parametros na url
 const jogadorId = urlsearchParams.get("id")
 
-//forms
+//div dos cards de receita extra e despesas
+const cardsreceitas = document.querySelector('#cardreceita')
+const cardsdespesas = document.querySelector('#carddespesa')
 
 //cards iniciais
 const totaljog = document.querySelector("#totalatletas")
@@ -74,10 +76,25 @@ async function getPagJog(id, ano) {
 
     })
 }
-window.onload = function(){
-    getPagMes()
+async function addDespesa(){
+    const nome = document.querySelector("#addnomed")
+    const dia = document.querySelector('#datadespesa')
+    const valor = document.querySelector('#addvalord')
+
+    let roudnovo = {
+        "id_pelada": 1,
+        "nome": nome.value,
+        "tipo": 'despesa',
+        "dia": dia.value,
+        "mes": '02',
+        "ano": '2023',
+        "valor": valor.value
+    };
+
+    roudnovo = JSON.stringify(roudnovo);
+    addRecOuDesp(roudnovo)
 }
-function addReceitaouDespesa(){
+async function addReceita(){
     const nome = document.querySelector("#addnome")
     const dia = document.querySelector('#datareceita')
     const valor = document.querySelector('#addvalor')
@@ -85,9 +102,9 @@ function addReceitaouDespesa(){
     let roudnovo = {
         "id_pelada": 1,
         "nome": nome.value,
-        "tipo": 'despesa',
+        "tipo": 'receita',
         "dia": dia.value,
-        "mes": 'fevereiro',
+        "mes": '02',
         "ano": '2023',
         "valor": valor.value
     };
@@ -104,4 +121,70 @@ async function addRecOuDesp(roudnovo) {
                 "Content-type": "application/json",
             },
         });
+
+    const receita_adicionada = await response.json();
+    if(receita_adicionada.tipo=='despesa'){
+        cardsdespesas.innerHTML = addHtmlRouC(receita_adicionada.nome, receita_adicionada.dia, receita_adicionada.mes, receita_adicionada.ano, receita_adicionada.valor, receita_adicionada.tipo) + cardsdespesas.innerHTML
+    }else{
+        cardsreceitas.innerHTML = addHtmlRouC(receita_adicionada.nome, receita_adicionada.dia, receita_adicionada.mes, receita_adicionada.ano, receita_adicionada.valor, receita_adicionada.tipo) + cardsreceitas.innerHTML
+    }
+    
+}
+async function percorreRecouDesp(){
+    const response = await fetch(url + 'receitas/')
+    const receitas = await response.json()
+
+    await receitas.map((receita)=>{
+        if(receita.id_pelada==1){
+            if(receita.tipo == 'despesa'){
+                cardsdespesas.innerHTML = addHtmlRouC(receita.nome, receita.dia, receita.mes, receita.ano, receita.valor, receita.tipo) + cardsdespesas.innerHTML
+            }else{
+                cardsreceitas.innerHTML = addHtmlRouC(receita.nome, receita.dia, receita.mes, receita.ano, receita.valor, receita.tipo) + cardsreceitas.innerHTML
+            }
+            
+        }
+    })
+}
+function addHtmlRouC(nome, dia, mes, ano, valor, tipo){
+    if(tipo == 'despesa'){
+        return `
+            <div class="card shadow mb-4">
+                <div class="card-header cent">
+                    <h6 class="m-0 font-weight-bold text-danger">${nome}</h6>
+                    <button type="button" class="btn btn-danger">X</button>
+                </div>
+                <div class="card-body">
+                    <div id="card-info">
+                        <strong><p>Data</p></strong>
+                        <strong><p>Valor</p></strong>
+                    </div>
+                    <div id="card-info">
+                        <p>${dia}/${mes}/${ano}</p>
+                        <p>R$${valor}</p>
+                    </div>
+                </div>
+            </div>`
+    }else{
+        return `
+            <div class="card shadow mb-4">
+                <div class="card-header cent">
+                    <h6 class="m-0 font-weight-bold text-success">${nome}</h6>
+                    <button type="button" class="btn btn-danger">X</button>
+                </div>
+                <div class="card-body">
+                    <div id="card-info">
+                        <strong><p>Data</p></strong>
+                        <strong><p>Valor</p></strong>
+                    </div>
+                    <div id="card-info">
+                        <p>${dia}/${mes}/${ano}</p>
+                        <p>R$${valor}</p>
+                    </div>
+                </div>
+            </div>`
+    }
+}
+window.onload = function(){
+    getPagMes()
+    percorreRecouDesp()
 }
